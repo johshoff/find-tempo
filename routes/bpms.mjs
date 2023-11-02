c2d712b15b64bdc207c5a359061e33dee992ea55
Johannes Hoff <johshoff@gmail.com>
2023-11-02 20:05:17 +0100

    Refactor spotify into separate file

--- routes/bpms.mjs
+++ routes/bpms.mjs
@@ -5,6 +5,7 @@ import https from 'https';
 import { getDatabase } from "../db.mjs";
 import { v4 as uuidv4 } from 'uuid';
 import bodyParser from 'body-parser';
+import { getTrackMeta } from '../spotify.mjs';
 
 var jsonParser = bodyParser.json()
 
@@ -59,81 +60,5 @@ router.post('/', jsonParser, async function(request, response) {
   response.writeHead(202, {"Access-Control-Allow-Origin": "http://johanneshoff.com"});
   response.end();
 });
-
-function trackLookupUrl(spotifyUri) {
-  const prefix = 'spotify:track:';
-  if (!spotifyUri || !spotifyUri.startsWith || !spotifyUri.startsWith(prefix)) {
-    return null;
-  }
-
-  const trackId = spotifyUri.slice(prefix.length);
-
-  return 'https://api.spotify.com/v1/tracks/' + trackId;
-}
-
-let cachedSpotifyCredentials = null;
-async function getSpotifyCredentials() {
-  if (cachedSpotifyCredentials) {
-    console.log('Using cached spotify credentials');
-    return cachedSpotifyCredentials;
-  }
-  console.log('Authenticating with Spotify');
-
-  const clientId = process.env['SPOTIFY_CLIENT_ID'];
-  const clientSecret = process.env['SPOTIFY_CLIENT_SECRET'];
-
-  if (!clientId || !clientSecret) {
-    throw 'Spotify credentials not set up (set with environment variables SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET)';
-  }
-
-  const tokenResult = await fetch("https://accounts.spotify.com/api/token", {
-    method: 'POST',
-    headers: new Headers({
-      'Content-type': 'application/x-www-form-urlencoded'
-    }),
-    body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
-  });
-  const credentials = await tokenResult.json();
-
-  if (credentials.expires_in && credentials.expires_in > 60) {
-    cachedSpotifyCredentials = credentials;
-    setTimeout(() => { cachedSpotifyCredentials = null; }, (credentials.expires_in - 60) * 1000)
-  }
-
-  return cachedSpotifyCredentials;
-}
-
-async function getSpotifyAuthHeaders() {
-  const credentials = await getSpotifyCredentials();
-  return { 'Authorization': `${credentials.token_type} ${credentials.access_token}` };
-}
-
-async function getTrackMeta(spotifyUri) {
-
-  const lookupUrl = trackLookupUrl(spotifyUri);
-
-  if (!lookupUrl) {
-    throw 'Invalid spotifyUri';
-  }
-  console.log(lookupUrl);
-
-  const response = await fetch(lookupUrl, { headers: new Headers(await getSpotifyAuthHeaders()) });
-
-  if (response.status !== 200) {
-    throw 'Failed to get artist and title';
-  }
-
-  try {
-    const track_info = await response.json();
-    console.log(track_info);
-    return {
-      artist: track_info.artists[0].name,
-      title:  track_info.name
-    };
-  }
-  catch (e) {
-    throw { message: 'Got response from spotify but failed to parse as expected', e };
-  }
-}
 
 export default router;
---
+++ spotify.mjs
@@ -0,0 +1,76 @@
+'use strict';
+
+function trackLookupUrl(spotifyUri) {
+  const prefix = 'spotify:track:';
+  if (!spotifyUri || !spotifyUri.startsWith || !spotifyUri.startsWith(prefix)) {
+    return null;
+  }
+
+  const trackId = spotifyUri.slice(prefix.length);
+
+  return 'https://api.spotify.com/v1/tracks/' + trackId;
+}
+
+let cachedSpotifyCredentials = null;
+async function getSpotifyCredentials() {
+  if (cachedSpotifyCredentials) {
+    console.log('Using cached spotify credentials');
+    return cachedSpotifyCredentials;
+  }
+  console.log('Authenticating with Spotify');
+
+  const clientId = process.env['SPOTIFY_CLIENT_ID'];
+  const clientSecret = process.env['SPOTIFY_CLIENT_SECRET'];
+
+  if (!clientId || !clientSecret) {
+    throw 'Spotify credentials not set up (set with environment variables SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET)';
+  }
+
+  const tokenResult = await fetch("https://accounts.spotify.com/api/token", {
+    method: 'POST',
+    headers: new Headers({
+      'Content-type': 'application/x-www-form-urlencoded'
+    }),
+    body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+  });
+  const credentials = await tokenResult.json();
+
+  if (credentials.expires_in && credentials.expires_in > 60) {
+    cachedSpotifyCredentials = credentials;
+    setTimeout(() => { cachedSpotifyCredentials = null; }, (credentials.expires_in - 60) * 1000).unref();
+  }
+
+  return cachedSpotifyCredentials;
+}
+
+async function getSpotifyAuthHeaders() {
+  const credentials = await getSpotifyCredentials();
+  return { 'Authorization': `${credentials.token_type} ${credentials.access_token}` };
+}
+
+export async function getTrackMeta(spotifyUri) {
+  const lookupUrl = trackLookupUrl(spotifyUri);
+
+  if (!lookupUrl) {
+    throw 'Invalid spotifyUri';
+  }
+  console.log(lookupUrl);
+
+  const response = await fetch(lookupUrl, { headers: new Headers(await getSpotifyAuthHeaders()) });
+
+  if (response.status !== 200) {
+    throw 'Failed to get artist and title';
+  }
+
+  try {
+    const track_info = await response.json();
+    console.log(track_info);
+    return {
+      artist: track_info.artists[0].name,
+      title:  track_info.name
+    };
+  }
+  catch (e) {
+    throw { message: 'Got response from spotify but failed to parse as expected', e };
+  }
+}
