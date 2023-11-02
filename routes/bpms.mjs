9f5e5bbefccd86142c836ce83357f9b54f2bbdb0
Johannes Hoff <johshoff@gmail.com>
2023-11-02 19:50:29 +0100

    Authenticate with Spotify
    This API used to be open, but that no longer works.

--- routes/bpms.mjs
+++ routes/bpms.mjs
@@ -71,15 +71,45 @@ function trackLookupUrl(spotifyUri) {
   return 'https://api.spotify.com/v1/tracks/' + trackId;
 }
 
-try{
-  console.log(await getTrackMeta("spotify:track:3NVLXCwpfZaCH00tJP5jUu"));
+let cachedSpotifyCredentials = null;
+async function getSpotifyCredentials() {
+  if (cachedSpotifyCredentials) {
+    console.log('Using cached spotify credentials');
+    return cachedSpotifyCredentials;
+  }
+  console.log('Authenticating with Spotify');
+
+  const clientId = process.env['SPOTIFY_CLIENT_ID'];
+  const clientSecret = process.env['SPOTIFY_CLIENT_SECRET'];
+
+  if (!clientId || !clientSecret) {
+    throw 'Spotify credentials not set up (set with environment variables SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET)';
+  }
+
+  const tokenResult = await fetch("https://accounts.spotify.com/api/token", {
+    method: 'POST',
+    headers: new Headers({
+      'Content-type': 'application/x-www-form-urlencoded'
+    }),
+    body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+  });
+  const credentials = await tokenResult.json();
+
+  if (credentials.expires_in && credentials.expires_in > 60) {
+    cachedSpotifyCredentials = credentials;
+    setTimeout(() => { cachedSpotifyCredentials = null; }, (credentials.expires_in - 60) * 1000)
+  }
+
+  return cachedSpotifyCredentials;
 }
-catch (e) {
-  console.error(e);
-  throw e;
+
+async function getSpotifyAuthHeaders() {
+  const credentials = await getSpotifyCredentials();
+  return { 'Authorization': `${credentials.token_type} ${credentials.access_token}` };
 }
 
 async function getTrackMeta(spotifyUri) {
+
   const lookupUrl = trackLookupUrl(spotifyUri);
 
   if (!lookupUrl) {
@@ -87,14 +117,10 @@ async function getTrackMeta(spotifyUri) {
   }
   console.log(lookupUrl);
 
-  const response = await fetch(lookupUrl);
-  if (response.statusCode !== 200) {
-    throw 'Failed to get artist and title';
-  }
+  const response = await fetch(lookupUrl, { headers: new Headers(await getSpotifyAuthHeaders()) });
 
-  const contentType = response.headers.get('content-type');
-  if (contentType != 'application/json') {
-    throw `Unexpected content type: ${contentType}`;
+  if (response.status !== 200) {
+    throw 'Failed to get artist and title';
   }
 
   try {
